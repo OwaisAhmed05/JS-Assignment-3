be4dd9dcf81a0faddda98aa7671e968873e4e5ed
Owais <owaisbusiness97@gmail.com>
2024-02-29 00:53:18 +0500

    first day work

---
+++ app.js
@@ -0,0 +1,111 @@
+// var a = 3
+// var b = 4
+// var res = a++ + ++a - b-- - --b + a++ + b++ - ++a + ++b
+//            3  +  5  -  4  -   2 +  5  +  2  -  7  +  4
+// document.write(res)
+
+
+// Assignment (2b) Task 1
+
+// var Name = prompt("Please Enter Your Name")
+
+// alert("Hi!" + Name)
+
+// Assignment (2b) Task 2
+
+// var city = prompt("Enter Your City")
+// alert("Welcome to the city of Lights")
+
+// Assignment (2b) Task 3
+
+// var gender = prompt("Please enter your gender!")
+
+// if (gender == "Male") {
+//     alert("Good Morning, Sir!")
+// }
+
+// else if (gender == "male") {
+//     alert("Good Morning, Sir!")
+// }
+
+// else if (gender == "Female") {
+//     alert("Good Morning, Ma'am!")
+// }
+
+// else if (gender == "female") {
+//     alert("Good Morning, Ma'am!")
+// }
+
+// Assignment (2b) Task 4
+
+// var signal = prompt("Please Enter Signal Colour")
+
+// if (signal == "red") {
+//     document.write("<table border='1px'><tr><th>Signal Colour</th><th>Message</th></tr></table><table border='1px'><tr><td>red</td><td>vehicle must stop</td></tr></table>")
+//     // alert("Vehicle must stop")
+// }
+
+// else if (signal == "yellow") {
+//     document.write("<table border='1px'><tr><th>Signal Colour</th><th>Message</th></tr></table><table border='1px'><tr><td>yellow</td><td>vehical should get ready to move</td></tr></table>")
+
+//     // alert("vehical should get ready to move")
+// }
+
+// else if (signal == "green") {
+//     document.write("<table border='1px'><tr><th>Signal Colour</th><th>Message</th></tr></table><table border='1px'><tr><td>green</td><td>vehicle can move now</td></tr></table>")
+
+//     // alert("vehicle can move now")
+// }
+
+// Assignment (2b) Task 5
+
+// var age1 = prompt("Maximum age")
+// var age2 = prompt("Current age")
+
+// if(age2 <= age1){
+//     alert("You are Welcome!")
+// }
+
+// Assignment (2b) Task 6
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+// }
+
+// // USER INPUT & CONDITIONAL STATEMENTS | JAVASCRIPT
+
+// // Page 5 of 12
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+// }
